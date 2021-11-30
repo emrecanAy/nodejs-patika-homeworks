72e1efd4eddc280c9fc29bc5c5017bda2220fb81
emrecanAy <codemrecan@gmail.com>
2021-12-01 02:13:37 +0300

    odev eklendi

--- homeworks/Odev5KendiWebSunucum/index.js
+++ homeworks/Odev5KendiWebSunucum/index.js
@@ -1,31 +1,29 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-
+    
     const url = req.url;
     if(url === '/'){
         res.writeHead(200, {'Content-Type':'text/html'});
-        res.write("<h1>INDEX SAYFASI</h1>");
+        res.write("<h2>Index Sayfasına Hoşgeldiniz!</h2>");
     }
-    else if(url === '/about'){
+    else if(url === '/hakkimda'){
         res.writeHead(200, {'Content-Type':'text/html'});
-        res.write("<h1>ABOUT SAYFASI</h1>");
+        res.write("<h2>Hakkımda Sayfasına Hoşgeldiniz!</h2>");
     }
-    else if(url === '/contact'){
+    else if(url === '/iletisim'){
         res.writeHead(200, {'Content-Type':'text/html'});
-        res.write("<h1>CONTACT SAYFASI</h1>");
+        res.write("<h2>İletişim Sayfasına Hoşgeldiniz!</h2>");
     }
     else{
         res.writeHead(404, {'Content-Type':'text/html'});
-        res.write("<h1>404 SAYFA BULUNAMADI!</h1>");
+        res.write("<h2>404 Sayfa Bulunamadı!</h2>");
     }
-
     console.log("Bir istek gönderildi!");
     res.end();
-}); 
+});
 
-const port = 3000;
+const port = 5000;
 server.listen(port, () => {
     console.log(`Sunucu port ${port}'de başlatıldı!`);
 });
-
